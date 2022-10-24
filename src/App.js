6f18c6363a31891b7efe43c31cad77426b4b332e
= <rickard_hallerback@hotmail.com>
2022-10-24 22:19:15 +0200

    Moving the L things into a component

--- src/App.js
+++ src/App.js
@@ -30,48 +30,13 @@ var handles = {
    push: push
 }
 
-function test(a) { a.hej = "hej" }
-
 function App() {
    var LSystemString = "L(g=0,a=0,c=0) -> mov(2,22/(g+1),2)rot(40)[L(g=g+1,a=a,c=c)]rot(-40)L(g=g+1, a=a, c=c)";
-   var LString = "mov(2,22/(g+1),2)rot(40)[L(g+1)]rot(-40)L(g+1)";
-
-   var a = { "hej": "test" };
-   test(a);
-
-   var result = "";
-   var lstring = LString;
-   try {
-      result = LParser(LSystemString);
-      lstring = result["rules"];
-   } catch (error) {
-      result = error;
-   }
-   console.log(result);
-   var lsystem = { state: { g: 0, a: 0, c: 0 }, lstring: lstring, stack: null };
-   Levolve(lsystem, 2);
-   console.log(lsystem);
-   Lexecute(lsystem, handles);
-   console.log(lsystem);
-   var lsystemExpanded = LexportAsString(lsystem);
-
-
-   // Test expanded string
-   var result2 = LParser(lsystemExpanded);
-   Lexecute({lstring:result2.value}, handles);
-
    return (
       <div className="App">
          <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <pre style={{ textAlign: "left" }}>
-               {JSON.stringify(lsystem, null, " ")}
-            </pre>
-            <p style={{ textAlign: "left" }}>
-               {lsystemExpanded}
-            </p>
-
-	    <LSystem lstring={lsystemExpanded} />
+	    <LSystem lsystemstring={LSystemString} lstring={LSystemString} depth={2} />
          </header>
       </div>
    );
--- src/components/Lsystem.jsx
+++ src/components/Lsystem.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-import Stack from '../stack.js';
+import { parse as LParser } from '../lsystem';
+import { Levolve, LexportAsString, Lexecute } from '../L';
+import { Stack } from '../stack.js';
 
 class LSystem extends Component {
 
@@ -10,12 +12,53 @@ class LSystem extends Component {
 			isFetching: false,
 			latest: null
 		};
+	  console.log("props", props);
+		this.stack = new Stack();
+		this.handles = {
+			pop: () => { this.pop(); },
+			push: () => { this.push(); },
+			rotate: (args) => { this.rotate(args); },
+			move: (args) => { this.move(args); }
+		};
 	}
 
 	render() {
-		return <p>"hej"</p>
+		var LSystemString = this.props.lsystemstring;
+		var result = LParser(LSystemString);
+		var lsystem = { state: { g: 0, a: 0, c: 0 }, lstring: result["rules"], stack: null };
+		Levolve(lsystem, this.props.depth);
+		var lsystemExpanded = LexportAsString(lsystem);
+
+		// Generating the instructions
+		var res = LParser(lsystemExpanded);
+		// Executing
+		Lexecute({ lstring: res.value }, this.handles);
+		return (
+		  <div>
+		    <pre style={{ textAlign: "left"}}>
+		      {JSON.stringify(lsystem, null, " ")}
+		    </pre>
+		    <p>
+		      {lsystemExpanded}
+		    </p>
+		  </div>);
 	}
 
+	push() {
+		console.log("my push");
+	};
+
+	pop() {
+		console.log("my pop");
+	};
+
+	rotate(args) {
+		console.log("rotate", args);
+	};
+
+	move(args) {
+		console.log("move", args);
+	};
 }
 
 export default LSystem;
