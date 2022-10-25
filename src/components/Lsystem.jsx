0aeca13d0ba0a0878949ab36f05e1b0c00cdd34c
= <rickard_hallerback@hotmail.com>
2022-10-25 22:19:38 +0200

    formatting, chosing depth

--- src/components/Lsystem.jsx
+++ src/components/Lsystem.jsx
@@ -10,62 +10,66 @@ class LSystem extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			LSystemString : this.props.lsystemstring,
-            currentText : this.props.lsystemstring,
-            lsystemExpanded : null
+			LSystemString: this.props.lsystemstring,
+			currentText: this.props.lsystemstring,
+			lsystemExpanded: null,
+			chosenDepth: 10
 		};
 		this.stack = new Stack();
-        this.handleNewLSystem = this.handleNewLSystem.bind(this);
-        this.handleNewInput = this.handleNewInput.bind(this);
+		this.handleNewLSystem = this.handleNewLSystem.bind(this);
+		this.handleNewInput = this.handleNewInput.bind(this);
 	}
 
-    componentDidMount() {
+	componentDidMount() {
 		var LSystemString = this.state.LSystemString;
 		var result = LParser(LSystemString);
 		var lsystem = { state: { g: 0, a: 0, c: 0 }, lstring: result["rules"], stack: null };
 		Levolve(lsystem, this.props.depth);
 		var lsystemExpanded = LexportAsString(lsystem);
-        this.setState({lsystemExpanded:lsystemExpanded});
-    }
+		this.setState({ lsystemExpanded: lsystemExpanded });
+	}
 
-    handleNewLSystem() {
-        var newLsystem = document.getElementById("LSystemTextArea").value;
+	handleNewLSystem() {
+		var newLsystem = document.getElementById("LSystemTextArea").value;
 		var LSystemString = newLsystem;
 		var result = LParser(LSystemString);
 		var lsystem = { state: { g: 0, a: 0, c: 0 }, lstring: result["rules"], stack: null };
-		Levolve(lsystem, this.props.depth);
+		Levolve(lsystem, this.state.chosenDepth);
 		var lsystemExpanded = LexportAsString(lsystem);
 
-        this.setState({
-            lsystemExpanded: lsystemExpanded,
-            LSystemString: newLsystem
-        });
-        console.log("new system, computed", newLsystem);
-    }
+		this.setState({
+			lsystemExpanded: lsystemExpanded,
+			LSystemString: newLsystem
+		});
+	}
 
-    handleNewInput(val) {
-        this.setState({
-            currentText: val.target.value
-        });
-    }
+	handleNewInput(val) {
+		this.setState({
+			currentText: val.target.value
+		});
+	}
 
 	render() {
-        if ( this.state.lsystemExpanded == null ) {
-            return "<p>Loading...</p>";
-        }
+		if (this.state.lsystemExpanded == null) {
+			return "<p>Loading...</p>";
+		}
 		return (
-		  <div>
-            <Turtle lstring={this.state.lsystemExpanded} />
-            <p>L system rule:</p>
-            <textarea style={{width:"20%"}}
-                rows="12"
-                id = "LSystemTextArea"
-                value={this.state.currentText}
-                onChange={this.handleNewInput} 
-            ></textarea><br/>
-            <button onClick={this.handleNewLSystem}>Generate</button>
-            <br/><br/>
-		  </div>);
+			<div>
+				<Turtle lstring={this.state.lsystemExpanded} />
+				<p>L system rule:</p>
+				<textarea style={{ width: "20%" }}
+					rows="12"
+					id="LSystemTextArea"
+					value={this.state.currentText}
+					onChange={this.handleNewInput}
+				></textarea><br />
+				<small>depth: </small>
+				<input type="number" style={{ width: "40px" }} value={this.state.chosenDepth} onChange={
+					e => {this.setState({ chosenDepth: e.target.value }); this.handleNewLSystem();}
+				}></input><br />
+				<button onClick={this.handleNewLSystem}>Generate</button>
+				<br /><br />
+			</div>);
 	}
 
 }
--- src/components/turtle.jsx
+++ src/components/turtle.jsx
@@ -19,70 +19,81 @@ class Turtle extends Component {
 			rotate: (args) => { this.rotate(args); },
 			move: (args) => { this.move(args); }
 		};
-        this.canvas = React.createRef();
-        this.ctx = false;
-        this.x = 0;
-        this.y = 0;
-        this.omega = -90;
+		this.canvas = React.createRef();
+		this.ctx = false;
+		this.x = 0;
+		this.y = 0;
+		this.omega = -90;
+		this.lstringExe = "";
+		this.drawThread = null;
+		this.draw = this.draw.bind(this);
 	}
 
-    componentDidMount() {
-        this.canvas = document.getElementById("canvas");
-        if ( this.canvas == null ) {
-            return;
-        }
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-        this.x = this.canvas.width / 2;
-        this.y = this.canvas.height * 0.9;
-        this.moveFactor = 0.001* this.canvas.height;
-        this.ctx = this.canvas.getContext("2d");
-        var Lstring = this.props.lstring;
+	draw() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.x = this.canvas.width / 2;
+		this.y = this.canvas.height * 0.9;	// Executing
+		Lexecute({ lstring: this.lstringExe }, this.handles);
+	}
+
+	componentDidMount() {
+		this.canvas = document.getElementById("canvas");
+		if (this.canvas == null) {
+			return;
+		}
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+
+		this.moveFactor = 0.001 * this.canvas.height;
+		this.ctx = this.canvas.getContext("2d");
 		// Generating the instructions
-		var res = LParser(Lstring);
-        console.log("lstring", Lstring);
-		// Executing
-		Lexecute({ lstring: res.value }, this.handles);
-    }
+		var res = LParser(this.props.lstring);
+		this.lstringExe = res.value;
+
+		if (this.drawThread != null) {
+			clearInterval(this.drawThread);
+		}
+		this.drawThread = setInterval(this.draw, 200);
+	}
 
 	render() {
-        this.componentDidMount();
+		this.componentDidMount();
 		return (
-            <div>
-            <canvas id="canvas" >
-            Your browser does not support the  HTML5 canvas element
-            </canvas>
-            </div>
-		  );
+			<div>
+				<canvas id="canvas" >
+					Your browser does not support the  HTML5 canvas element
+				</canvas>
+			</div>
+		);
 	}
 
 	push() {
-        this.stack.push({x: this.x, y: this.y, omega: this.omega});
+		this.stack.push({ x: this.x, y: this.y, omega: this.omega });
 	};
 
 	pop() {
-        var st = this.stack.pop();
-        this.x = st.x;
-        this.y = st.y;
-        this.omega = st.omega;
+		var st = this.stack.pop();
+		this.x = st.x;
+		this.y = st.y;
+		this.omega = st.omega;
 	};
 
 	rotate(args) {
-        this.omega += args[0];
+		this.omega += args[0];
 	};
 
 	move(args) {
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.x, this.y);
-        var newX = this.x + args[0] * this.moveFactor * Math.cos((this.omega/360.0) * Math.PI*2);
-        var newY = this.y + args[0] * this.moveFactor * Math.sin((this.omega/360.0) * Math.PI*2);
+		this.ctx.beginPath();
+		this.ctx.moveTo(this.x, this.y);
+		var newX = this.x + args[0] * this.moveFactor * Math.cos((this.omega / 360.0) * Math.PI * 2);
+		var newY = this.y + args[0] * this.moveFactor * Math.sin((this.omega / 360.0) * Math.PI * 2);
 
-        this.ctx.lineWidth = args[1];
-        this.ctx.lineTo(newX, newY);
-        this.ctx.closePath();
-        this.ctx.stroke();
-        this.x = newX;
-        this.y = newY;
+		this.ctx.lineWidth = args[1];
+		this.ctx.lineTo(newX, newY);
+		this.ctx.closePath();
+		this.ctx.stroke();
+		this.x = newX;
+		this.y = newY;
 	};
 
 }
